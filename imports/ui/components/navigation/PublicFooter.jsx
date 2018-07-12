7c05d0817096c1b564c540b6a4fd1997517bbf35
Diego Robles <diego@hemployeed.com>
2018-07-11 22:00:42 -0400

    improved public footer

--- imports/ui/components/navigation/PublicFooter.jsx
+++ imports/ui/components/navigation/PublicFooter.jsx
@@ -1,3 +1,43 @@
 import React from "react";
 
-export default (PublicFooter = props => <h1>PublicFooter</h1>);
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = theme => ({
+  container: {
+    minHeight: "15vh",
+    backgroundColor: theme.palette.primary.dark,
+    padding: theme.spacing.unit
+  },
+  footerLink: {
+    color: `${theme.palette.secondary.light} !important`,
+    "&:hover": {
+      color: "white !important"
+    }
+  }
+});
+
+const PublicFooter = ({ classes }) => (
+  <Grid
+    container
+    className={classes.container}
+    direction="column"
+    justify="center"
+    alignItems="center"
+  >
+    <Typography variant="caption" color="secondary">
+      Developed by
+    </Typography>
+    <Button
+      variant="contained"
+      color="secondary"
+      href="https://www.noincomedev.me"
+    >
+      NOINCOMEDEV
+    </Button>
+  </Grid>
+);
+
+export default withStyles(styles, { withTheme: true })(PublicFooter);
